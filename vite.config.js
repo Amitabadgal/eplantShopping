a6eccb17fe73403589322a9151d303b14495f2d8
Amitabadgal <amitamita4@gmail.com>
2024-09-23 18:19:32 +0530

    update vit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact", // This base path is useful if you are deploying under a subdirectory
+  base: "/", // This base path is useful if you are deploying under a subdirectory
   plugins: [react()],
   build: {
     outDir: 'dist', // Ensure the output directory is 'dist'
